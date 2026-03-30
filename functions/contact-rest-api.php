543688b0188135fd8dca552f65258b4efc56e503
corsacca <corsacca@gmail.com>
2026-03-30 10:57:54 +0100

    Add marketing consent to the contact us form

--- functions/contact-rest-api.php
+++ functions/contact-rest-api.php
@@ -59,6 +59,7 @@ function doxa_handle_contact_form( WP_REST_Request $request ) {
     $email   = sanitize_email( $params['email'] ?? '' );
     $country = sanitize_text_field( $params['country'] ?? '' );
     $message = sanitize_textarea_field( $params['message'] ?? '' );
+    $consent_doxa_general = ! empty( $params['consent_doxa_general'] );
 
     // Validate required fields
     if ( empty( $email ) || empty( $message ) ) {
@@ -75,10 +76,11 @@ function doxa_handle_contact_form( WP_REST_Request $request ) {
 
     $response = wp_remote_post( rtrim( $api_url, '/' ) . '/api/contact', [
         'body'    => wp_json_encode( [
-            'name'    => $name,
-            'email'   => $email,
-            'country' => $country,
-            'message' => $message,
+            'name'                 => $name,
+            'email'                => $email,
+            'country'              => $country,
+            'message'              => $message,
+            'consent_doxa_general' => $consent_doxa_general,
         ] ),
         'headers' => [
             'X-API-Key'    => $api_key,
